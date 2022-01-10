49c4a64546245d87df5fa257863704ac35daec56
fahd <fahdarft@gmail.com>
2022-01-10 18:32:53 +0300

    Remove all logic from api file

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,27 +1,23 @@
 import express from 'express';
 import resizeImage from '../../utils/resizeImage';
-import path from 'path';
+// import path from 'path';
 
 const images = express.Router();
-const options = {
-  root: path.join(__dirname, '../../../images/resized'),
-  headers: {
-    'x-timestamp': Date.now(),
-    'x-sent': true,
-  },
-};
 
-images.get('/', (req, res) => {
-  resizeImage(
-    req.query.filename as string,
-    req.query.height as string,
-    req.query.width as string
-  ).then(() => {
-    res.sendFile(`resized-${req.query.filename}.jpg`, options, (err) => {
-      console.log(err);
-      res.end();
-    });
-  });
-});
+images.use('/', resizeImage);
+
+
+// images.get('/', (req, res) => {
+//   resizeImage(
+//     req.query.filename as string,
+//     req.query.height as string,
+//     req.query.width as string
+//   ).then(() => {
+//     res.sendFile(`resized-${req.query.filename}.jpg`, options, (err) => {
+//       console.log(err);
+//       res.end();
+//     });
+//   });
+// });
 
 export default images;
--- src/utils/resizeImage.ts
+++ src/utils/resizeImage.ts
@@ -1,21 +1,56 @@
+import express from 'express';
 import sharp from 'sharp';
 import path from 'path';
 
-const resizeImage = async (
-  fileName: string,
-  height: string,
-  width: string
-): Promise<void> => {
-  try {
-    const h: number = parseInt(height);
-    const w: number = parseInt(width);
-    await sharp(path.join(__dirname, `../../images/full/${fileName}.jpg`))
-      .resize(w, h)
-      .toFile(
-        path.join(__dirname, `../../images/resized/resized-${fileName}.jpg`)
-      );
-  } catch (err) {
-    console.log(err);
-  }
+// const resizeImage = async (
+//   fileName: string,
+//   height: string,
+//   width: string
+// ): Promise<void> => {
+//   try {
+//     const h: number = parseInt(height);
+//     const w: number = parseInt(width);
+//     await sharp(path.join(__dirname, `../../images/full/${fileName}.jpg`))
+//       .resize(w, h)
+//       .toFile(
+//         path.join(__dirname, `../../images/resized/resized-${fileName}.jpg`)
+//       );
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+const filePath = path.join(__dirname, `../../images/full/`);
+const resizedFilePath = path.join(__dirname, `../../images/resized/`);
+
+//options parameter for sendFile function
+const options = {
+  root: path.join(__dirname, '../../images/resized/'),
+  headers: {
+    'x-timestamp': Date.now(),
+    'x-sent': true,
+  },
 };
+
+const resizeImage = async (req:express.Request, res:express.Response, next:Function):Promise<void>  => {
+  const fileName = req.query.filename ? req.query.filename : undefined;
+  const height = parseInt(req.query.height as  string) ? parseInt(req.query.height as string): undefined;
+  const width = parseInt(req.query.width as  string) ? parseInt(req.query.width as string): undefined ;
+  if( fileName && height && width) {
+    try {
+      console.log(fileName, height, width);
+      await sharp(`${filePath}${fileName}.jpg`).resize(width, height).toFile(`${resizedFilePath}resized-${fileName}.jpg`);
+      res.sendFile(`resized-${fileName}.jpg`, options, () => {
+        next();
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  } else {
+    console.log(req.query);
+    console.log(fileName, height, width);
+    next();
+  }
+}
+
+
 export default resizeImage;
